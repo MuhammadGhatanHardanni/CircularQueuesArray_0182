e05d5709be2fe04b29e73e3ee5bc62f39a7e3481
Ghatan55 <ghatanhardanni@gmail.com>
2025-06-12 11:01:55 +0700

    Menambahakan menu pilihan menggunakan while true

--- CircularQueues.cpp
+++ CircularQueues.cpp
@@ -122,5 +122,23 @@ void insert()
 };
 
 int main() {
-    
+    Queues q;
+    char ch;
+
+    while (true) {
+        try {
+            cout << "Menu" << endl;
+            cout << "1. Implement insert operation" << endl;
+            cout << "2. Implement delete operation" << endl;
+            cout << "3. Display Values" << endl;
+            cout << "4. Exit" << endl;
+            cout << "Enter your choice (1-4): ";
+            cin >> ch;
+            cout << endl;
+
+         
+        }
+    }
+
+    return 0;
 }
