3ecedeeb3177390c7402a90442d3a0542629fba9
Ghatan55 <ghatanhardanni@gmail.com>
2025-06-12 10:55:46 +0700

    Mengecek apakah antrian kosong pada void display

--- CircularQueues.cpp
+++ CircularQueues.cpp
@@ -72,6 +72,18 @@ void insert()
 
     void display()
     {
+        int FRONT_position = FRONT;
+        int REAR_position = REAR;
+
+        // cek apakah antrian kosong
+        if (FRONT == - 1)
+        {
+            cout << "Queue is Empty\n";
+            return;
+        }
+
+        cout << "\nElement in the queue are..\n";
+
         
     }
 
